a19c1815a325be0dbb5a68983a7feb621246e2b2
Wojciech Rak <Rak.wojtek@wp.pl>
2021-05-26 18:48:36 +0200

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,1 +1,9 @@
-Test
+data class Person(val name: String, val surname: String, val age: Int)
+{
+    fun getPerson() = "$name $surname, age $age"
+}
+
+fun main() {
+    val person = Person("John", "Doe", 21)
+    println(person.getPerson())
+}
